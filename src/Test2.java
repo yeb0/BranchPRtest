e65e158977807edb5c1107eb0508cd10c1bb08d5
yeb0 <s110528@nate.com>
2023-06-01 14:23:39 +0900

    test2 is push

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+    System.out.println("test2");
+    System.out.println("test2 push ~!");
+    System.out.println();
+  }
+}
